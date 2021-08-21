922dcc97ece1c706310a861f5a37ab59199b3621
xieguigang <xie.guigang@gcmodeller.org>
2021-08-22 01:59:27 +0800

    read cdf data

--- MS-Imaging/app.ts
+++ MS-Imaging/app.ts
@@ -5,5 +5,13 @@ function loadNetCDF(url: string, render: (r: MSIRender) => void) {
 }
 
 function createMSIRender(cdf: NetCDFReader): MSIRender {
+    const mz: number[] = <any>cdf.getDataVariable("mz");
+    const intensity: number[] = <any>cdf.getDataVariable("intensity");
+    const x: number[] = <any>cdf.getDataVariable("x");
+    const y: number[] = <any>cdf.getDataVariable("y");
+    const pts = mz.map(function (mzi, i) {
+        [mzi, intensity[i], x[i], y[i]];
+    });
+
 
 }
--- dist/ms-imaging.js
+++ dist/ms-imaging.js
@@ -12,6 +12,13 @@ function loadNetCDF(url, render) {
     NetCDFReader.fetch(url, function (cdf) { return render(createMSIRender(cdf)); });
 }
 function createMSIRender(cdf) {
+    var mz = cdf.getDataVariable("mz");
+    var intensity = cdf.getDataVariable("intensity");
+    var x = cdf.getDataVariable("x");
+    var y = cdf.getDataVariable("y");
+    var pts = mz.map(function (mzi, i) {
+        [mzi, intensity[i], x[i], y[i]];
+    });
 }
 var mzPack = /** @class */ (function () {
     function mzPack() {
